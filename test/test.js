bcf2d576a8febe525a06ae93328f01622d7d365b
ClutteredMind <clutteredmind@users.noreply.github.com>
2015-09-30 23:23:43 -0700

    Adding unit tests
    Just a couple of very basic unit tests for the HTTP server.

---
+++ test/test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+require = require('really-need');
+var request = require('supertest');
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('base server functionality', function (){
+	var server;
+	beforeEach(function (){
+		server = require('../app.js', { bustCache: true });
+	});
+	
+	it('responds to /', function (done){
+		request(server)
+			.get('/')
+			.expect(200, done);
+	});
+	
+	it('gives a 404 for everything else', function (done){
+		request(server)
+			.get('/everything/else')
+			.expect(404, done);
+	});
+});
