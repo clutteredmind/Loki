bcc69c092db6f8c1537e1691b2a6935c1495714e
ClutteredMind <clutteredmind@users.noreply.github.com>
2015-10-23 20:09:51 -0700

    Reorganized tests

--- test/test_addons.js
+++ test/test_addons.js
@@ -11,105 +11,119 @@ describe('addon tests', function () {
 
 	describe('ProcessListAddon', function () {
 		var addon_name = 'ProcessListAddon';
-		var addon = null;
-		
-		beforeEach(function () {
-			addon = require(path.join(base_addon_path, addon_name));
-		});
 
-		it('should exist', function () {
-			addon.should.not.be.null;
-		});
-		
-		it('should return a wrapped object with "new"', function () {
-			var process_list_addon = new addon.ProcessListAddon ();
-			process_list_addon.should.not.be.null;
-		});
-		
-		describe('functions', function () {
-			var process_list_addon;
+		describe('addon', function () {
+			var addon = null;
+
 			beforeEach(function () {
-				process_list_addon = new addon.ProcessListAddon ();
+				addon = require(path.join(base_addon_path, addon_name));
 			});
-			
-			describe('getProcesses', function () {
-				it('should be a function', function () {
-					process_list_addon.getProcesses.should.be.a.Function;
+
+			it('should exist', function () {
+				addon.should.not.be.null;
+			});
+
+			describe('ProcessListAddon', function () {
+				var process_list_addon;
+				beforeEach(function () {
+					process_list_addon = new addon.ProcessListAddon();
 				});
-				it('should return an array', function () {
-					(process_list_addon.getProcesses()).should.be.an.Array;
+
+				it('should return a wrapped object with "new"', function () {
+					process_list_addon.should.be.an.Object;
+				});
+
+				describe('functions', function () {
+					describe('getProcesses', function () {
+						it('should be a function', function () {
+							process_list_addon.getProcesses.should.be.a.Function;
+						});
+						it('should return an array', function () {
+							(process_list_addon.getProcesses()).should.be.an.Array;
+						});
+					});
 				});
 			});
 		});
 	});
-	
+
 	describe('DeviceListAddon', function () {
 		var addon_name = 'DeviceListAddon';
-		var addon = null;
-		
-		beforeEach(function () {
-			addon = require(path.join(base_addon_path, addon_name));
-		});
 
-		it('should exist', function () {
-			addon.should.not.be.null;
-		});
-		
-		it('should return a wrapped object with "new"', function () {
-			var device_list_addon = new addon.DeviceListAddon ();
-			device_list_addon.should.not.be.null;
-		});
-		
-		describe('functions', function () {
-			var device_list_addon;
+		describe('addon', function () {
+			var addon = null;
+
 			beforeEach(function () {
-				device_list_addon = new addon.DeviceListAddon();
+				addon = require(path.join(base_addon_path, addon_name));
 			});
-			describe('getDevices', function () {
-				it('should be a function', function () {
-					device_list_addon.getDevices.should.be.a.Function;
+
+			it('should exist', function () {
+				addon.should.not.be.null;
+			});
+
+			describe('DeviceListAddon', function () {
+				var device_list_addon = null;
+				beforeEach(function () {
+					device_list_addon = new addon.DeviceListAddon();
 				});
-				it('should return an array', function () {
-					(device_list_addon.getDevices()).should.be.an.Array;
+
+				it('should return a wrapped object with "new"', function () {
+					device_list_addon.should.be.an.Object;
+				});
+
+				describe('functions', function () {
+					describe('getDevices', function () {
+						it('should be a function', function () {
+							device_list_addon.getDevices.should.be.a.Function;
+						});
+						it('should return an array', function () {
+							(device_list_addon.getDevices()).should.be.an.Array;
+						});
+					});
 				});
 			});
 		});
 	});
-	
+
 	describe('ScreenshotAddon', function () {
 		var addon_name = 'ScreenshotAddon';
-		var addon = null;
-		
-		beforeEach(function () {
-			addon = require(path.join(base_addon_path, addon_name));
-		});
-		
-		it('should exist', function () {
-			addon.should.not.be.null;
-		});
-		
-		it('should return a wrapped object with "new"', function () {
-			var screenshot_addon = new addon.ScreenshotAddon();
-			screenshot_addon.should.not.be.null;
-		});
-		
-		describe('functions', function () {
-			var screenshot_addon;
+
+		describe('addon', function () {
+			var addon = null;
+
 			beforeEach(function () {
-				screenshot_addon = new addon.ScreenshotAddon();
+				addon = require(path.join(base_addon_path, addon_name));
 			});
-			describe('captureScreen', function () {
-				it('should be a function', function () {
-					screenshot_addon.captureScreen.should.be.a.Function;
+
+			it('should exist', function () {
+				addon.should.not.be.null;
+			});
+
+			describe('ScreenshotAddon', function () {
+				var screenshot_addon = null;
+				beforeEach(function () {
+					screenshot_addon = new addon.ScreenshotAddon();
 				});
-				it('should throw an exception if called with no arguments', function () {
-					expect(screenshot_addon.captureScreen.bind(screenshot_addon)).to.throw("Invalid parameter count. Expected one function parameter.");
+
+				it('should return a wrapped object with "new"', function () {
+					screenshot_addon.should.be.an.Object;
 				});
-				it('should throw an exception if called with an argument that is not a function', function () {
-					expect(screenshot_addon.captureScreen.bind(screenshot_addon, true)).to.throw("The parameter must be a function.");
-				});
-				it('should return "undefined" when called with a callback', function () {
-					expect(screenshot_addon.captureScreen(function () {})).to.be.Undefined;
+
+				describe('functions', function () {
+					describe('captureScreen', function () {
+						it('should be a function', function () {
+							screenshot_addon.captureScreen.should.be.a.Function;
+						});
+						it('should throw an exception if called with no arguments', function () {
+							expect(screenshot_addon.captureScreen.bind(screenshot_addon)).to.throw("Invalid parameter count. Expected one function parameter.");
+						});
+						it('should throw an exception if called with an argument that is not a function', function () {
+							expect(screenshot_addon.captureScreen.bind(screenshot_addon, true)).to.throw("The parameter must be a function.");
+						});
+						it('should return "undefined" when called with a callback', function () {
+							expect(screenshot_addon.captureScreen(function () { })).to.be.Undefined;
+						});
+					});
 				});
 			});
 		});
