4a29fb998ee1891617131e86f391a7fea168c311
ClutteredMind <yutaka@clutteredmind.com>
2017-03-01 21:34:02 -0800

    More HTML changes
    Switched to AdminLTE widgets
    Fixed the header link to correctly use the router instead of reloading
    the page
    Added a home link to the sidebar

--- dist/app/components/site-header/site-header.component.ts
+++ dist/app/components/site-header/site-header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component }    from '@angular/core';
+import { Router }       from '@angular/router';
 
 @Component({
     moduleId: module.id,
@@ -6,4 +7,7 @@ import { Component } from '@angular/core';
     templateUrl: './site-header.component.html'
 })
 export class SiteHeaderComponent {
+    constructor(private router: Router) {
+
+    }
 }
